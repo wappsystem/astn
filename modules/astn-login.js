edd1693438cb44cbc5a223c7434597c2261114f1
Gunnar Unger <gunnar.unger@gmail.com>
2019-03-13 12:21:25 +1100

    update

--- modules/astn-login.js
+++ modules/astn-login.js
@@ -1,9 +1,9 @@
 (function(){
     var modules={
-        "home":                         {"url":"$A/astn/modules/online-questionnaire-login.html","Table":"astn-shq-participant-20002391",
+        "home":                         {"url":"$A/modules/online-questionnaire-login.html","Table":"astn-shq-participant-20002391",
                                                 questionnaire_panel:"online-questionnaire-panel"
                                         }   ,
-        "online-questionnaire-panel":   {url:"$A/astn/modules/online-questionnaire-panel.html"},
+        "online-questionnaire-panel":   {url:"$A/modules/online-questionnaire-panel.html"},
     }
     for(m in modules){$vm.module_list[m]=modules[m];}
 })();
